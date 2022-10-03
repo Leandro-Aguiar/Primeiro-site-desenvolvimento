f7f828eb3658a52762589c6e23e7e62c5db8cd4e
Leandro-Aguiar <leosaguiarl@yahoo.com.br>
2022-10-03 18:42:51 -0300

    Concluindo javascript login

---
+++ css/script_login.js
@@ -0,0 +1,13 @@
+function btenviar(){
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
+    
+
+    if (email.length >= 3 && senha.length >= 3){
+        alert("Dados enviados, aguarde...")
+
+    }
+
+
+
+}
